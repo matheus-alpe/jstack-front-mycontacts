4661804d3efde7f3b41821c6e61a2890586336e6
Matheus Alves Pereira <matttalves@gmail.com>
2022-12-12 20:10:55 -0300

    feat: form

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,45 +1,75 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 import Button from '../Button';
 import FormGroup from '../FormGroup';
 import Input from '../Input';
 import Select from '../Select';
 
-import { Form } from './styles';
+import { Container } from './styles';
 
 export default function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log({
+      name, email, phone, category
+    });
+  }
+
   return (
-    <Form>
-      <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
+    <form onSubmit={handleSubmit}>
+      <Container>
+        <FormGroup>
+          <Input
+            placeholder="Nome"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </FormGroup>
 
-      <FormGroup
-        error="O formato do e-mail é inválido."
-      >
-        <Input placeholder="E-mail" error />
-      </FormGroup>
+        <FormGroup>
+          <Input
+            type="email"
+            placeholder="E-mail"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </FormGroup>
 
-      <FormGroup>
-        <Input placeholder="Telefone" />
-      </FormGroup>
+        <FormGroup>
+          <Input
+            placeholder="Telefone"
+            value={phone}
+            onChange={(event) => setPhone(event.target.value)}
+          />
+        </FormGroup>
 
-      <FormGroup>
-        <Select placeholder="Categoria">
-          <option value="instagram">Instagram</option>
-          <option value="facebook">Facebook</option>
-          <option value="linkedin">LinkedIn</option>
-          <option value="twitter">Twitter</option>
-        </Select>
-      </FormGroup>
+        <FormGroup>
+          <Select
+            placeholder="Categoria"
+            value={category}
+            onChange={(event) => setCategory(event.target.value)}
+          >
+            <option value="" disabled>Categoria</option>
+            <option value="instagram">Instagram</option>
+            <option value="facebook">Facebook</option>
+            <option value="linkedin">LinkedIn</option>
+            <option value="twitter">Twitter</option>
+          </Select>
+        </FormGroup>
 
-      <Button
-        type="submit"
-        disabled
-      >
-        {buttonLabel}
-      </Button>
-    </Form>
+        <Button
+          type="submit"
+        >
+          {buttonLabel}
+        </Button>
+      </Container>
+    </form>
   );
 }
 
--- src/components/ContactForm/styles.js
+++ src/components/ContactForm/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-export const Form = styled.form`
+export const Container = styled.div`
   display: flex;
   flex-direction: column;
   gap: 16px;
